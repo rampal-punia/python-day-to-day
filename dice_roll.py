76c03981d69cb6c1614d41f1217ba10d2b61be4b
Coding Mantras <rampalsinghpunia@gmail.com>
2023-01-28 22:28:54 +0530

    Add dice_roll

---
+++ dice_roll.py
@@ -0,0 +1,17 @@
+'''✨ Python: Dice Roll Outcomes ✨ 
+🎲🎲🎲🎲🎲🎲🎲🎲
+
+Get random output from a dice-roll
+'''
+import random
+
+# Possible outcomes
+outcomes = (1, 2, 3, 4, 5, 6)
+
+# Method-1: Using choice from random module
+score = random.choice(tuple(outcomes))
+print(score)
+
+# Method-2: Using randint from random module
+score = random.randint(1, 6)
+print(score)
