7cedf171e5b133eafb50357c36bbceeec581e773
Coding Mantras <rampalsinghpunia@gmail.com>
2023-01-28 22:21:00 +0530

    Add comma_in_python

---
+++ comma_in_python/demo_2.py
@@ -0,0 +1,33 @@
+# For more on Python follow: https://twitter.com/CodingMantras
+
+
+'''✨ More than a bracket () a comma ',' decides
+a tuple in Python ✨
+'''
+
+
+# Question:
+
+t = (1, 2, 3)
+
+# What would be the output of this print statement
+print(type(t))
+
+# Ans: <class 'tuple'>
+# Correct
+
+# Now
+t = 1, 2, 3
+
+# What would be the output of this print statement
+print(type(t))
+
+# Ans: <class 'tuple'>
+
+# And also
+t = 1,
+
+print(type(t))
+# Ans: <class 'tuple'>
+
+# I hope you were not expecting the class int here!
---
+++ comma_in_python/readme.md
@@ -0,0 +1,10 @@
+# Two different use cases of comma in Python
+
+1. ✨ The comma is not an operator in Python, but a separator between expressions.
+
+2. ✨ More than a bracket () a comma ',' decides a tuple in Python.
+
+## Support me
+
+- Give this repo a star
+- follow: [@CodingMantras](https://twitter.com/CodingMantras) on Twitter.
