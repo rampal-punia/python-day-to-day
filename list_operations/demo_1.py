4fbba8c71c05aa28bfa00e9375cadaaf70206309
Coding Mantras <rampalsinghpunia@gmail.com>
2023-01-28 22:35:33 +0530

    Add list_ops

---
+++ list_operations/demo_1.py
@@ -0,0 +1,10 @@
+'''✨ Python: Flatten a Nested List ✨'''
+
+nested = [[1, 2], [3, 4], [5, 6]]
+# Required Output: [1, 2, 3, 4, 5, 6]
+
+flat = [i for sublist in nested for i in sublist]
+print(flat)
+
+
+# For more on Python follow: https://twitter.com/CodingMantras
