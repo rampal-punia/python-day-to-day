d10e1bad9c78c539f06dd8e14a7115c02e92ec12
Coding Mantras <rampalsinghpunia@gmail.com>
2023-01-29 08:40:49 +0530

    Add capitalize_and_title

---
+++ operations_on_strings/demo_1.py
@@ -0,0 +1,16 @@
+'''✨ capitalize() VS title() ✨
+
+What is the difference between capitalize() and title()
+methods of Python String?
+'''
+
+
+# Only first letter of the sentence is capital, rest lowercase.
+print("learning PYTHON is Fun".capitalize())
+# Output: Learning python is fun
+
+# The first letter of all the words is capital.
+print("learning PYTHON is Fun".title())
+# Output: Learning Python Is Fun
+
+# For more on Python follow: https://twitter.com/CodingMantras
