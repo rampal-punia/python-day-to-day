41f789e9cd0c9c0171ab4824a0c0290246485308
Coding Mantras <rampalsinghpunia@gmail.com>
2023-02-20 21:52:08 +0530

    Update quiz_8

---
+++ python_quizzes/quiz_9.py
@@ -0,0 +1,24 @@
+'''✨ Python Quiz: Difference of Sets ✨ 
+
+What would be the behaviour of this code?
+
+A) Only Method A will work
+B) Only Method B will work
+C) Both methods (A & B) will work
+D) Both methods will generate an Error
+'''
+my_set = {1, 2, 3}
+my_list = [3, 4, 5]
+
+# Method 🅰️
+result = my_set.difference(my_list)
+
+# Method 🅱️
+result = my_set - my_list
+
+
+######################################################################
+# Answer:-
+
+
+# For more on Python follow: https://twitter.com/CodingMantras
