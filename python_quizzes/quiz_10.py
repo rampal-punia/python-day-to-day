9a2b98e6bca9080c35075e174264b471d086cac8
Coding Mantras <rampalsinghpunia@gmail.com>
2023-02-26 09:32:30 +0530

    Add quiz 10

---
+++ python_quizzes/quiz_10.py
@@ -0,0 +1,24 @@
+'''✨ Python Quiz: Remove an element from set ✨ 
+
+What would be the behaviour of this code?
+
+A) Only Method A will work
+B) Only Method B will work
+C) Both methods (A & B) will work
+D) Both methods will generate an Error
+'''
+languages = {"Python", "Java", "JavaScript", "C++", "Ruby"}
+
+
+# Method 🅰️
+result = my_set.difference(my_list)
+
+# Method 🅱️
+result = my_set - my_list
+
+
+######################################################################
+# Answer:-
+
+
+# For more on Python follow: https://twitter.com/CodingMantras
