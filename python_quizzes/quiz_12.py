0aefa0f7ebddff48adcbf844bed0a8571559b194
Coding Mantras <rampalsinghpunia@gmail.com>
2023-02-27 18:55:54 +0530

    update

---
+++ python_quizzes/quiz_12.py
@@ -0,0 +1,22 @@
+'''✨ Python Quiz: difference_update() method ✨ 
+
+What is the result of the following code snippet?
+A) {1, 2, 3, 4, 5, 6, 7}
+B) {1, 2}
+C) {3, 4, 5}
+D) {6, 7}
+'''
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+set1.difference_update(set2)
+print(set1)
+
+# Output: ??? 💭
+
+
+######################################################################
+# Answer:- B
+{1, 2}
+
+
+# For more on Python follow: https://twitter.com/CodingMantras
