eeff77e0f86c8d83bb88868bc44e32cb3786d701
Coding Mantras <rampalsinghpunia@gmail.com>
2023-03-02 18:42:07 +0530

    Update quiz 14

---
+++ python_quizzes/quiz_14.py
@@ -0,0 +1,18 @@
+'''✨ Python Quiz: Using round() function ✨ 
+
+What is the result of the following code snippet?
+A) n
+B) nohtyP evol I
+C) Error
+'''
+
+print("I love Python"[::-1])
+
+# Output: ??? 💭
+
+
+######################################################################
+# Answer:- B
+
+
+# For more on Python follow: https://twitter.com/CodingMantras
