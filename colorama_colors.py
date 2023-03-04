e4f230b8566c35e706a47654cb55dd0661dd2718
Coding Mantras <rampalsinghpunia@gmail.com>
2023-03-04 08:51:58 +0530

    update colorama color

---
+++ colorama_colors.py
@@ -0,0 +1,10 @@
+'''✨ Python Package: Colorama 
+Produce colored terminal text and Background.✨'''
+
+from colorama import Fore, Back
+
+print(Back.WHITE + "Set the background color to White!")
+
+print(Fore.RED + "This text will appear in Red!")
+print(Fore.GREEN + "This text will appear in Green!")
+print(Fore.BLUE + "This text will appear in Blue!")
