59b59150d7632a4fba6b0473ac90d26ef3a50459
Coding Mantras <rampalsinghpunia@gmail.com>
2023-03-06 20:49:03 +0530

    Add random_choice

---
+++ random_choice.py
@@ -0,0 +1,13 @@
+'''✨ Python: Random Choice ✨ '''
+
+import random
+
+sunday_options = ["Python Coding", "Outing", "Movie", "Songs"]
+choice = random.choice(sunday_options)
+
+print(choice)
+
+# Why am I getting "Python Coding" every time
+
+
+# For more on Python follow: https://twitter.com/CodingMantras
